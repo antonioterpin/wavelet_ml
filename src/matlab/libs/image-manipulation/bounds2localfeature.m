07b13c133386cae5c07d1f7e7d5adac37d354bab
Antonio Terpin <antonio.terpin@gmail.com>
2019-09-30 10:52:55 +0200

    libs documentation

--- src/matlab/libs/image-manipulation/bounds2localfeature.m
+++ src/matlab/libs/image-manipulation/bounds2localfeature.m
@@ -1,14 +1,10 @@
-function [shape_feature,local_feature] = bounds2localfeature(encoded_shape_feature,bounding_box,image,image_size)
+function [shape_feature,local_feature] = bounds2localfeature(encoded_shape_feature,bounding_box,image)
 %ALPHA_SHAPE Given the region bounds returns both shape and local
 %features.
 %   [shape_feature,local_feature] = bounds2localfeature(encoded_shape_feature,bounding_box,image)
-%   [shape_feature,local_feature] = bounds2localfeature(encoded_shape_feature,bounding_box,image,image_size)
-%   Use * image = false * to get only shape feature
 % See also SEGMENTATE_IMAGE, ALPHA_SHAPE
 
-    if nargin == 3
-        image_size = size(image);
-    end
+    image_size = size(image);
     shape_feature = zeros(image_size); local_feature = zeros(image_size); % black image
     [~,decoded_displaced_shape_feature] = rle_decoding(encoded_shape_feature,image_size);
     % center bounds
@@ -20,7 +16,7 @@ function [shape_feature,local_feature] = bounds2localfeature(encoded_shape_featu
     shape_feature(displacement_vector(2):displacement_vector(2) + maxY - minY,...
         displacement_vector(1):displacement_vector(1) + maxX - minX) = ...
         decoded_displaced_shape_feature(minY:maxY,minX:maxX);
-    if image && size(image,1) > maxY && size(image,2) > maxX
+    if image_size(1) >= maxY && image_size(2) >= maxX
         local_feature(displacement_vector(2):displacement_vector(2) + maxY - minY,...
             displacement_vector(1):displacement_vector(1) + maxX - minX) = ...
             image(minY:maxY,minX:maxX);
