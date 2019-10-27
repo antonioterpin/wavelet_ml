2b53120eb749d8584347f14e762bca251b8504a9
claverau <claudioverardo@gmail.com>
2019-10-27 10:41:29 +0100

    Region proposal updated

--- src/matlab/architecture/region_proposal/segmentation_opt.m
+++ src/matlab/architecture/region_proposal/segmentation_opt.m
@@ -1,34 +1,44 @@
 
-% parameters
-class = 3;
-batch_size = 40;
+function segmentation_opt_results = segmentation_opt(class, batch_size)
 
-% hyperparameters to optimize
-hyst_tl = optimizableVariable('hyst_tl',[0 60],'Type','integer');
-hyst_th = optimizableVariable('hyst_th',[60 255],'Type','integer');
-alpha = optimizableVariable('alpha',[5 20],'Type','integer');
-hole_th = optimizableVariable('hole_th',[500 5000],'Type','integer');
-region_th = optimizableVariable('region_th',[500 5000],'Type','integer');
+    % hyperparameters to optimize
+    kov_nscale = optimizableVariable('kov_nscale',[3 6],'Type','integer');
+    kov_norient = optimizableVariable('kov_norient',[4 16],'Type','integer');
+    kov_min_wl = optimizableVariable('kov_min_wl',[2 5],'Type','real');
+    kov_mult = optimizableVariable('kov_mult',[1.1 3],'Type','real');
+    hyst_tl = optimizableVariable('hyst_tl',[5 80],'Type','integer');
+    % hyst_tl = optimizableVariable('hyst_tl',[0 0.6],'Type','real');
+    hyst_th = optimizableVariable('hyst_th',[80 255],'Type','integer');
+    % hyst_th = optimizableVariable('hyst_th',[0 0.6],'Type','real');
+    alpha = optimizableVariable('alpha',[5 20],'Type','integer');
+    hole_th = optimizableVariable('hole_th',[500 15000],'Type','integer');
+    region_th = optimizableVariable('region_th',[500 15000],'Type','integer');
 
-hyp_segmentation = [hyst_tl, hyst_th, alpha, hole_th, region_th];
+    hyp_segmentation = [kov_nscale, kov_norient, kov_min_wl, kov_mult, hyst_tl, hyst_th, alpha, hole_th, region_th];
 
-% dataset images
-global global_feature_images
-images = imageDatastore(convertStringsToChars(global_feature_images),...
-          'IncludeSubfolders', true, 'LabelSource', 'foldernames'); % use foldernames as labels
+    % dataset images
+    global global_feature_images
+    images = imageDatastore(convertStringsToChars(global_feature_images),...
+              'IncludeSubfolders', true, 'LabelSource', 'foldernames'); % use foldernames as labels
 
-% dataset csv
-global augmented_dataset_path
-dataset = readtable(augmented_dataset_path, 'Delimiter', ',');
+    % dataset csv
+    global augmented_dataset_path
+    dataset = readtable(augmented_dataset_path, 'Delimiter', ',');
 
-% bayesian optimization
-segmentation_opt_results = bayesopt(@(params)err_segmentation(params, images, dataset, class, batch_size),...
-                                    hyp_segmentation,...
-                                    'AcquisitionFunctionName','expected-improvement-plus',...
-                                    'Verbose',1);
-                            
-hyp_segmentation_opt = segmentation_opt_results.XAtMinObjective;     
-loss_segmentation_opt = segmentation_opt_results.MinObjective;     
+    % bayesian optimization
+    segmentation_opt_results = bayesopt(@(params)err_segmentation(params, images, dataset, class, batch_size),...
+                                        hyp_segmentation,...
+                                        'AcquisitionFunctionName','expected-improvement-plus',...
+                                        'MaxObjectiveEvaluations',50,...
+                                        'UseParallel',true,...
+                                        'Verbose',1);
+                                    
+%                                         'XConstraintFcn',@hyst_constraint,...
+
+    % hyp_segmentation_opt = segmentation_opt_results.XAtMinObjective;     
+    % loss_segmentation_opt = segmentation_opt_results.MinObjective;
+
+end
 
 % objective function evaluation
 function err = err_segmentation(params, images, dataset, class, batch_size)
@@ -59,27 +69,43 @@ function err = err_segmentation(params, images, dataset, class, batch_size)
         [filepath,name,ext] = fileparts(cell2mat(images.Files(i)));
         filename = strcat(name,ext);
         
+        % ACTUAL REGIONS
         encoded_correct_pixels = cell2mat(dataset{find(strcmp(filename,dataset{:,1})),class+1});
         
         % bounding box correct pixels
-        [~, map] = rle_decoding(encoded_correct_pixels, im_size);
-        [number_of_regions, ~, bounding_boxes] = segmentate_image(map);
-        correct_pixels = zeros(im_size);                                                                 
-        for region_id = 1:number_of_regions
-            correct_pixels(bounding_boxes(1,2,region_id):bounding_boxes(2,2,region_id), ...
-                         bounding_boxes(1,1,region_id):bounding_boxes(2,1,region_id)) = 1;
-        end                      
-        encoded_correct_pixels = rle_encoding(correct_pixels);
+        % [~, map] = rle_decoding(encoded_correct_pixels, im_size);
+        % [number_of_regions, ~, bounding_boxes] = segmentate_image(map);
+        % correct_pixels = zeros(im_size);                                                                 
+        % for region_id = 1:number_of_regions
+        %     correct_pixels(bounding_boxes(1,2,region_id):bounding_boxes(2,2,region_id), ...
+        %                  bounding_boxes(1,1,region_id):bounding_boxes(2,1,region_id)) = 1;
+        % end                      
+        % encoded_correct_pixels = rle_encoding(correct_pixels);
         
-        %imshow(correct_pixels);
+        % imshow(correct_pixels);
     
-        % region proposal
-        map = defect_edge_detection(im, params.hyst_tl, params.hyst_th);
+        % REGION PROPOSAL
+        
+        % edge detection
+        map = defect_edge_detection(im,...
+                                    params.kov_nscale,...
+                                    params.kov_norient,...
+                                    params.kov_min_wl,...
+                                    params.kov_mult,...
+                                    params.hyst_tl,...
+                                    params.hyst_th);
+        
+        map(1:end,1) = 1;
+        map(1,1:end) = 1;
+        map(end, 1:end) = 1;
+        map(1:end, end) = 1;
+        
+        % segmentation
         [number_of_regions, encoded_shape_maps, bounding_boxes, shp] = segmentate_image(map,...
                                                                                         params.alpha,...
                                                                                         params.hole_th,...
                                                                                         params.region_th);
-        
+        % bounding box
         im_segmented = zeros(im_size);                                      
         for region_id = 1:number_of_regions
             im_segmented(bounding_boxes(1,2,region_id):bounding_boxes(2,2,region_id), ...
@@ -88,10 +114,18 @@ function err = err_segmentation(params, images, dataset, class, batch_size)
                                            
         encoded_proposed_pixels = rle_encoding(im_segmented);
         
+        % LOSS FUNCTION
+        
         err = err + 1 - loss_function(encoded_proposed_pixels, encoded_correct_pixels, im_size);
         
     end
     
     err = err ./ batch_size;
     
+end
+
+function tf = hyst_constraint(X)
+
+    tf = X.hyst_tl < X.hyst_th;
+
 end
