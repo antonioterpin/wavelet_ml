30a26d7b91db3dc20da5afaa52a31cb410c22795
claverau <claudioverardo@gmail.com>
2019-10-27 22:38:46 +0100

    Segmentation toolbox updated

--- src/matlab/architecture/region_proposal/segmentation_configuration.m
+++ src/matlab/architecture/region_proposal/segmentation_configuration.m
@@ -5,10 +5,15 @@ global augmented_dataset_path
 dataset = readtable(augmented_dataset_path,'Delimiter', ',');
 
 % images
-global global_feature_preprocessed_images
-global global_feature_images
-images = imageDatastore(convertStringsToChars(global_feature_images),...
-                       'IncludeSubfolders', true, 'LabelSource', 'foldernames'); % use foldernames as labels
+global global_feature_preprocessed_images global_feature_images
+% w/o equalization
+% images = imageDatastore(convertStringsToChars(global_feature_images),...
+%                        'IncludeSubfolders', true,...
+%                        'LabelSource', 'foldernames'); % use foldernames as labels
+% w/ equalization
+images = imageDatastore(convertStringsToChars(global_feature_preprocessed_images),...
+                       'IncludeSubfolders', true,...
+                       'LabelSource', 'foldernames'); % use foldernames as labels
 
 % names of images
 [~,name,ext] = cellfun(@fileparts,images.Files,'UniformOutput',false);
@@ -18,7 +23,8 @@ clear('name','ext');
 % highlighted images
 global defects_highlighted
 images_high = imageDatastore(convertStringsToChars(defects_highlighted),...
-                       'IncludeSubfolders', true, 'LabelSource', 'foldernames'); % use foldernames as labels
+                            'IncludeSubfolders', true,...
+                            'LabelSource', 'foldernames'); % use foldernames as labels
               
 % names of highlighted images
 [~,name,ext] = cellfun(@fileparts,images_high.Files,'UniformOutput',false);
