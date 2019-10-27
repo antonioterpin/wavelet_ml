ea6c9d55c64f44072f6eeedc163eaf57c6ca0265
claverau <claudioverardo@gmail.com>
2019-10-27 22:38:46 +0100

    Segmentation toolbox updated

--- src/matlab/architecture/region_proposal/defect_edge_detection.m
+++ src/matlab/architecture/region_proposal/defect_edge_detection.m
@@ -1,12 +1,11 @@
-function map = defect_edge_detection(im, nscale, norient, minWaveLength, mult, tl, th)
-% function map = defect_edge_detection(im,tl,th)
+function map = defect_edge_detection(im, kov_nscale, kov_norient, kov_min_wl, kov_mult, hyst_tl, hyst_th)
 
-    M = phasecong3(im, nscale, norient, minWaveLength, mult);
+    M = phasecong3(im, kov_nscale, kov_norient, kov_min_wl, kov_mult);
     M_scaled = ( M - min(min(M)) ) ./ ( max(max(M)) - min(min(M)) ) .* 255;
     
     % figure; imshow(M);
     
-    M_scaled_clean = hysthresh(M_scaled,tl,th);
+    M_scaled_clean = hysthresh(M_scaled, hyst_tl, hyst_th);
     
     map = M_scaled_clean;
     map(map>0) = 1;
