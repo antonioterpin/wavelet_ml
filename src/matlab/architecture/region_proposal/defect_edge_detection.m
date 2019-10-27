131d7237c4e696bd519dcb8b89bec079f1b710ee
claverau <claudioverardo@gmail.com>
2019-10-27 10:41:29 +0100

    Region proposal updated

--- src/matlab/architecture/region_proposal/defect_edge_detection.m
+++ src/matlab/architecture/region_proposal/defect_edge_detection.m
@@ -1,8 +1,11 @@
-function map = defect_edge_detection(im,tl,th)
+function map = defect_edge_detection(im, nscale, norient, minWaveLength, mult, tl, th)
+% function map = defect_edge_detection(im,tl,th)
 
-    M = phasecong3(im);
+    M = phasecong3(im, nscale, norient, minWaveLength, mult);
     M_scaled = ( M - min(min(M)) ) ./ ( max(max(M)) - min(min(M)) ) .* 255;
-
+    
+    % figure; imshow(M);
+    
     M_scaled_clean = hysthresh(M_scaled,tl,th);
     
     map = M_scaled_clean;
