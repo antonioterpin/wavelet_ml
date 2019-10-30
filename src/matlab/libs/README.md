de0e4a9d8c9c09f891b37e55d8456ca2a27ff9ea
Antonio Terpin <antonio.terpin@gmail.com>
2019-10-30 14:54:42 +0100

    Libs update
    Documentation
    Combinate utils function

--- src/matlab/libs/README.md
+++ src/matlab/libs/README.md
@@ -2,6 +2,7 @@
 This folder contains all utilities functions shared among different files and folders.
 - globals.m
 - loss_function.m
+- combinate.m
 - image-manipulation
 - rle
 
@@ -10,6 +11,9 @@ This file is used to keep all global constants.
 
 ### loss_function.m
 The considered metric for object detection in images.
+
+### combinate.m
+A util function to repeat a given function on the combination of N elements.
 
 ### rle
 This [folder](./rle) contains all the code useful to encode and decode binary matrices.
--- src/matlab/libs/loss_function.m
+++ src/matlab/libs/loss_function.m
@@ -2,8 +2,9 @@ function score = loss_function(encoded_proposed_pixels, encoded_correct_pixels, 
 % LOSS_FUNCTION object detection considered metric.
 %
 %   score = LOSS_FUNCTION(proposed_pixels, correct_pixels) given the 
-%   proposed_pixels (X) and the correct_pixels (Y), gives the score: 
-%   score = 2 * |X ? Y| / (|X| + |Y|)
+%   Let X be the proposed_pixels set and Y be the correct_pixels set. Then:
+%                   
+%   score = 2 * |intersect(X, Y)| / (|X| + |Y|)
 
     proposed_pixels = rle_decoding(encoded_proposed_pixels, domain_size);
     correct_pixels = rle_decoding(encoded_correct_pixels, domain_size);
