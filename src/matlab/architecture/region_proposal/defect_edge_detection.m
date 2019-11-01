da39afaecad9accbd90fb041f6c4b85f25f31f91
claver <claudioverardo@gmail.com>
2019-11-01 23:25:30 +0100

    Segmentation script updated

--- src/matlab/architecture/region_proposal/defect_edge_detection.m
+++ src/matlab/architecture/region_proposal/defect_edge_detection.m
@@ -3,7 +3,7 @@ function map = defect_edge_detection(im, kov_nscale, kov_norient, kov_min_wl, ko
     M = phasecong3(im, kov_nscale, kov_norient, kov_min_wl, kov_mult);
     M_scaled = ( M - min(min(M)) ) ./ ( max(max(M)) - min(min(M)) ) .* 255;
     
-    % figure; imshow(M);
+    % figure('Position', [0 400 1600 256]); colormap(gray(256)); imagesc(M_scaled); set(gca,'xtick',[],'ytick',[]);
     
     M_scaled_clean = hysthresh(M_scaled, hyst_tl, hyst_th);
     
