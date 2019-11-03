15fa1967295e3d40c47ee9f15245977058c2516a
Antonio Terpin <antonio.terpin@gmail.com>
2019-11-03 14:04:29 +0100

    Structure local & shape column

--- src/matlab/architecture/mc-cnn/structure_shape_column.m
+++ src/matlab/architecture/mc-cnn/structure_shape_column.m
@@ -6,27 +6,29 @@ function layers = structure_shape_column()
 %   See also STRUCTURE_LOCAL_COLUMN, STRUCTURE_GLOBAL_COLUMN
 
     layers = [
-    imageInputLayer([256 1600 1])
+    imageInputLayer([256 1600 1],"Name","imageinput")
     
-    convolution2dLayer(7,8,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
+    convolution2dLayer([3 3],4,"Name","shuffle","DilationFactor",[400 400],"Padding",[672 672 0 0])
     
-    maxPooling2dLayer(2,'Stride',2)
+    convolution2dLayer([3 3],8,"Name","conv1","Padding","same")
+    maxPooling2dLayer([4 4],"Name","mp1","Padding","same","Stride",[4 4])
     
-    convolution2dLayer(5,16,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
+    convolution2dLayer([3 3],16,"Name","conv2","Padding","same")
+    maxPooling2dLayer([4 4],"Name","mp2","Padding","same","Stride",[4 4])
     
-    maxPooling2dLayer(2,'Stride',2)
+    convolution2dLayer([3 3],32,"Name","conv3","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp3","Padding","same","Stride",[2 2])
     
-    convolution2dLayer(3,32,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
+    convolution2dLayer([3 3],64,"Name","conv4","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp4","Padding","same","Stride",[2 2])
     
-    maxPooling2dLayer(2,'Stride',2)
+    convolution2dLayer([3 3],128,"Name","conv5","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp5","Padding","same","Stride",[2 2])
     
-    fullyConnectedLayer(4)
-    softmaxLayer
-    classificationLayer];
+    fullyConnectedLayer(16,"Name","fc1")
+    fullyConnectedLayer(16,"Name","fc2")
+    fullyConnectedLayer(4,"Name","fc3")
+    
+    softmaxLayer("Name","softmax")
+    classificationLayer("Name","classoutput")];
 end
