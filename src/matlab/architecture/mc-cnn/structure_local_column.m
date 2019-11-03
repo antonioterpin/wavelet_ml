be846596014eb42d395116a89df4f4e7d4d69ca8
Antonio Terpin <antonio.terpin@gmail.com>
2019-11-03 14:04:29 +0100

    Structure local & shape column

--- src/matlab/architecture/mc-cnn/structure_local_column.m
+++ src/matlab/architecture/mc-cnn/structure_local_column.m
@@ -8,44 +8,26 @@ function layers = structure_local_column()
     layers = [
     imageInputLayer([256 1600 1],"Name","imageinput")
     
-    convolution2dLayer([11 1],2,"Name","conv_1")
-    convolution2dLayer([1 11],2,"Name","conv_2")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_1")
-    maxPooling2dLayer([3 3],"Name","maxpool_1","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],4,"Name","shuffle","DilationFactor",[400 400],"Padding",[672 672 0 0])
     
-    convolution2dLayer([7 1],2,"Name","conv_5")
-    convolution2dLayer([1 7],2,"Name","conv_6")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_3")
-    maxPooling2dLayer([3 3],"Name","maxpool_3","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],8,"Name","conv1","Padding","same")
+    maxPooling2dLayer([4 4],"Name","mp1","Padding","same","Stride",[4 4])
     
-    convolution2dLayer([5 1],2,"Name","conv_3")
-    convolution2dLayer([1 5],2,"Name","conv_4")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_2")
-    maxPooling2dLayer([3 3],"Name","maxpool_2","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],16,"Name","conv2","Padding","same")
+    maxPooling2dLayer([4 4],"Name","mp2","Padding","same","Stride",[4 4])
     
-    convolution2dLayer([3 1],3,"Name","conv_7")
-    convolution2dLayer([1 3],3,"Name","conv_8")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_4")
-    maxPooling2dLayer([3 3],"Name","maxpool_4","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],32,"Name","conv3","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp3","Padding","same","Stride",[2 2])
     
-    convolution2dLayer([3 1],3,"Name","conv_9")
-    convolution2dLayer([1 3],3,"Name","conv_10")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_5")
-    maxPooling2dLayer([3 3],"Name","maxpool_5","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],64,"Name","conv4","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp4","Padding","same","Stride",[2 2])
     
-    convolution2dLayer([1 1],3,"Name","conv_11")
-    convolution2dLayer([1 3],3,"Name","conv_12")
-    batchNormalizationLayer
-    leakyReluLayer(0.01,"Name","leakyrelu_6")
-    maxPooling2dLayer([3 3],"Name","maxpool_6","Padding","same","Stride",[2 2])
+    convolution2dLayer([3 3],128,"Name","conv5","Padding","same")
+    maxPooling2dLayer([2 2],"Name","mp5","Padding","same","Stride",[2 2])
     
-    fullyConnectedLayer(7,"Name","fc_1")
-    fullyConnectedLayer(4,"Name","fc_2")
+    fullyConnectedLayer(16,"Name","fc1")
+    fullyConnectedLayer(16,"Name","fc2")
+    fullyConnectedLayer(4,"Name","fc3")
     
     softmaxLayer("Name","softmax")
     classificationLayer("Name","classoutput")];
